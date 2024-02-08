fcfcb530114c35633b6c157a5228d02fe7f041b9
Andrey Laschishin <laschishin@yandex.ru>
2024-02-08 23:12:10 +0100

    hw01 first commit

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,11 +1,16 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -13,11 +18,48 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
+
+        String testFileName = fileNameProvider.getTestFileName();
+
         // Использовать CsvToBean
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+
+
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        InputStream is = getFileAsStream(testFileName);
+
+        List<QuestionDto> beans;
+        try (Reader r = new InputStreamReader(is)) {
+            beans = new CsvToBeanBuilder(r)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .build()
+                    .parse();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Question> questions = beans.stream()
+                .map(bean -> bean.toDomainObject())
+                .collect(Collectors.toList());
+
+        return questions;
+    }
+
+    private InputStream getFileAsStream(String fileName){
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName); // TODO развернуть и посмотреть что будет с несуществующим файлом
+
+        if(inputStream == null){
+            throw new IllegalArgumentException("File not found: " + fileName);
+        }
+
+        return inputStream;
+
     }
 }
--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,26 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
+    private final CsvQuestionDao csvQuestionDao;
 
     @Override
     public void executeTest() {
-        ioService.printLine("");
+
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        List<Question> questions = csvQuestionDao.findAll();
+
+        questions.stream().forEach(question -> ioService.printFormattedLine(String.valueOf(question)));
+
+//        ioService.printFormattedLine(String.valueOf(questions.get(0)));
     }
 }
