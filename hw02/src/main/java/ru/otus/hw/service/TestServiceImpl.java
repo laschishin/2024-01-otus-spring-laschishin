5bdfe554e85dccc33da6ba0f8a417b8a3dfec0e9
Andrey Laschishin <laschishin@yandex.ru>
2024-02-15 20:01:53 +0100

    hw02

---
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final CsvQuestionDao csvQuestionDao;
+
+    private final QuestionService questionService;
+
+    @Override
+    public void executeTest() {
+
+        ioService.printFormattedLine("Please answer the questions below%n");
+
+        List<Question> questions = csvQuestionDao.findAll();
+
+        for (Question question : questions) {
+            questionService.print(question);
+        }
+
+    }
+
+}
