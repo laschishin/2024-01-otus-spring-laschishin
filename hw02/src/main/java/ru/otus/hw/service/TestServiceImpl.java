58af8df3b1a42cf5fe139d5eddc362aa872601f1
andrey.laschishin <andrey.laschishin@gefasoft-engineering.com>
2024-02-16 14:31:36 +0100

    hw02

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,7 +3,10 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
 
 import java.util.List;
 
@@ -13,21 +16,38 @@ public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
-    private final CsvQuestionDao csvQuestionDao;
+    private final QuestionDao questionDao;
 
     private final QuestionService questionService;
 
     @Override
-    public void executeTest() {
+    public TestResult executeTestFor(Student student) {
 
         ioService.printFormattedLine("Please answer the questions below%n");
 
-        List<Question> questions = csvQuestionDao.findAll();
+        List<Question> questions = questionDao.findAll();
+
+        var testResult = new TestResult(student);
 
         for (Question question : questions) {
             questionService.print(question);
         }
 
+        return testResult;
     }
 
+
+//    @Override
+//    public TestResult executeTestFor(Student student) {
+//        ioService.printLine("");
+//        ioService.printFormattedLine("Please answer the questions below%n");
+//        var questions = questionDao.findAll();
+//        var testResult = new TestResult(student);
+//
+//        for (var question: questions) {
+//            var isAnswerValid = false; // Задать вопрос, получить ответ
+//            testResult.applyAnswer(question, isAnswerValid);
+//        }
+//        return testResult;
+//    }
 }
